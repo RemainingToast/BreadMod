97a530831ce59578657ace89e21aa9530d4a3e6f
RemainingToast <remainingtoast@gmail.com>
2021-01-15 01:58:17 +1000

    Added Armour Items and Models, Textures coming.

--- src/main/java/com/github/remainingtoast/BreadMod.java
+++ src/main/java/com/github/remainingtoast/BreadMod.java
@@ -1,18 +1,32 @@
 package com.github.remainingtoast;
 
+import com.github.remainingtoast.armor.BreadArmourMaterial;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BreadMod implements ModInitializer {
 
-	public static final Item BREAD_HELMET = new Item(new Item.Settings().group(ItemGroup.COMBAT).maxCount(1));
+	private static final ItemGroup BREAD_GROUP = new ItemGroup(4, "bread") {
+		@Override
+		public ItemStack createIcon() {
+			return new ItemStack(Items.BREAD);
+		}
+	};
+
+	public static final Item BREAD_HELMET = new ArmorItem(BreadArmourMaterial.BREAD, EquipmentSlot.HEAD, new Item.Settings().group(BREAD_GROUP));
+	public static final Item BREAD_CHEST = new ArmorItem(BreadArmourMaterial.BREAD, EquipmentSlot.CHEST, new Item.Settings().group(BREAD_GROUP));
+	public static final Item BREAD_LEGS = new ArmorItem(BreadArmourMaterial.BREAD, EquipmentSlot.LEGS, new Item.Settings().group(BREAD_GROUP));
+	public static final Item BREAD_BOOTS = new ArmorItem(BreadArmourMaterial.BREAD, EquipmentSlot.FEET, new Item.Settings().group(BREAD_GROUP));
 
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("breadmod", "bread_helmet"), BREAD_HELMET);
+		Registry.register(Registry.ITEM, new Identifier("breadmod", "bread_chest"), BREAD_CHEST);
+		Registry.register(Registry.ITEM, new Identifier("breadmod", "bread_legs"), BREAD_LEGS);
+		Registry.register(Registry.ITEM, new Identifier("breadmod", "bread_boots"), BREAD_BOOTS);
 
 		System.out.println("Hello Bread World!");
 	}
---
+++ src/main/java/com/github/remainingtoast/armor/BreadArmourMaterial.java
@@ -0,0 +1,74 @@
+package com.github.remainingtoast.armor;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Lazy;
+
+import java.util.function.Supplier;
+
+public enum BreadArmourMaterial implements ArmorMaterial {
+
+    BREAD("bread", 20, new int[]{ 2, 4, 5, 2},10, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 1.0F, 0.5F,() -> {
+        return Ingredient.ofItems(Items.BREAD);
+    });
+
+    private static final int[] BASE_DURABILITY = new int[]{13, 15, 16, 11};
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] protectionAmounts;
+    private final int enchantability;
+    private final SoundEvent equipSound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Lazy<Ingredient> repairIngredientSupplier;
+
+    BreadArmourMaterial(String name, int durabilityMultiplier, int[] protectionAmounts, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredientSupplier) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmounts = protectionAmounts;
+        this.enchantability = enchantability;
+        this.equipSound = equipSound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredientSupplier = new Lazy(repairIngredientSupplier);
+    }
+
+    public int getDurability(EquipmentSlot slot) {
+        return BASE_DURABILITY[slot.getEntitySlotId()] * this.durabilityMultiplier;
+    }
+
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return this.protectionAmounts[slot.getEntitySlotId()];
+    }
+
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    public SoundEvent getEquipSound() {
+        return this.equipSound;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredientSupplier.get();
+    }
+
+    @Environment(EnvType.CLIENT)
+    public String getName() {
+        return this.name;
+    }
+
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
